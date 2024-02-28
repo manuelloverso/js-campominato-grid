fac2bb6f6a523003f4469fa42d190b1dc7bdcadb
manuelloverso <manuelloverso@gmail.com>
2024-02-28 15:14:43 +0100

    add cells with js fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,8 @@ const cellMarkup = `<div class="square"></div>`;
 // Generating 100 cells
 const container = document.getElementById("container");
 //console.log(container);
+for (let i = 0; i < 100; i++) {
+  container.insertAdjacentHTML("beforeend", cellMarkup);
+}
+
+//Giving each cell a number
