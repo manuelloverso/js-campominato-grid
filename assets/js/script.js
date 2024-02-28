30d508d4fdf933eae4b08531372259318406d33e
manuelloverso <manuelloverso@gmail.com>
2024-02-28 17:14:11 +0100

    empty container before generating a second grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,19 @@
 //Variables Declaration
 const cellMarkup = `<div class="square"></div>`;
 const playBtn = document.getElementById("play");
-console.log(playBtn);
-const container = document.getElementById("container");
-console.log(container);
+//console.log(playBtn);
+let container = document.getElementById("container");
+//console.log(container);
 const selection = document.getElementById("difficulty");
-console.log(selection);
+//console.log(selection);
 
 // Generating cells based on difficulty
 playBtn.addEventListener("click", function () {
-  if (selection.value == "easy") {
-    generateEasy();
-  } else if (selection.value == "mid") {
-    generateMid();
-  } else {
-    generateHard();
-  }
+  generateGrid();
 
   //Giving each cell a number
   const cellsArray = document.querySelectorAll(".square");
-  console.log(cellsArray);
+  //console.log(cellsArray);
 
   for (let i = 0; i < cellsArray.length; i++) {
     const element = cellsArray[i];
@@ -44,31 +38,37 @@ function selectedCell() {
 }
 
 //Function to generate 100 cells
-function generateEasy() {
-  for (let i = 0; i < 100; i++) {
-    container.insertAdjacentHTML("beforeend", cellMarkup);
+function generateGrid() {
+  if (container.childNodes.length > 0) {
     const singleCell = document.querySelectorAll(".square");
-    console.log(singleCell);
-    singleCell[i].classList.add("easy");
-  }
-}
 
-//Function to generate 81 cells
-function generateMid() {
-  for (let i = 0; i < 81; i++) {
-    container.insertAdjacentHTML("beforeend", cellMarkup);
-    const singleCell = document.querySelectorAll(".square");
-    console.log(singleCell);
-    singleCell[i].classList.add("mid");
+    for (let i = 0; i < singleCell.length; i++) {
+      container.removeChild(singleCell[i]);
+    }
   }
-}
+  if (selection.value == "easy") {
+    for (let i = 0; i < 100; i++) {
+      container.insertAdjacentHTML("beforeend", cellMarkup);
+      const singleCell = document.querySelectorAll(".square");
 
-//Function to generate 49 cells
-function generateHard() {
-  for (let i = 0; i < 49; i++) {
-    container.insertAdjacentHTML("beforeend", cellMarkup);
-    const singleCell = document.querySelectorAll(".square");
-    console.log(singleCell);
-    singleCell[i].classList.add("hard");
+      //console.log(singleCell);
+      singleCell[i].classList.add("easy");
+    }
+  } else if (selection.value == "mid") {
+    for (let i = 0; i < 81; i++) {
+      container.insertAdjacentHTML("beforeend", cellMarkup);
+
+      const singleCell = document.querySelectorAll(".square");
+      //console.log(singleCell);
+      singleCell[i].classList.add("mid");
+    }
+  } else {
+    for (let i = 0; i < 49; i++) {
+      container.insertAdjacentHTML("beforeend", cellMarkup);
+
+      const singleCell = document.querySelectorAll(".square");
+      //console.log(singleCell);
+      singleCell[i].classList.add("hard");
+    }
   }
 }
